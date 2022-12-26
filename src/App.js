52fed5ab525f6fe8b6e69ab4378965ffe47496c1
BhavyaJain711 <bhavyajain711@gmail.com>
2022-12-26 07:27:58 +0530

    removed carousel from app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import CarouselExample from "./components/Carousel";
 import CardPage from "./pages/CardPage.jsx"
 import data from "./components/Card/CardData"
 function App() {
-  
-      
+    
   return (
     <div className="App">
-      {/* <CarouselExample /> */}
       <CardPage data={data} />
     </div>
   );
