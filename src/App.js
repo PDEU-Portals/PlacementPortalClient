f5a67ae15c9fd0b4b4747660d9951c22c53a63d2
Preet Sojitra <preet.dev373@gmail.com>
2023-01-21 17:43:23 +0530

    feat(quizApp): add quiz app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import CardPage from "./pages/CardPage.jsx"
 import data from "./components/Card/CardData"
 import HomePage from "./pages/HomePage"
-import { ProfilePage } from "./pages/ProfilePage.jsx";
-import { Route, Routes } from "react-router-dom";
+import {ProfilePage} from "./pages/ProfilePage.jsx"
+import {Route, Routes} from "react-router-dom"
+import QuizApp from "./pages/QuizApp.jsx"
 // import { Profile } from "./components/Profile Page Component/Profile.jsx";
 function App() {
-    
   return (
     <div className="App">
       <Routes>
@@ -13,9 +13,10 @@ function App() {
         <Route path="/dashboard" element={<div>Dashboard</div>} />
         <Route path="/student" element={<ProfilePage />} />
         <Route path="/community" element={<CardPage data={data} />} />
+        <Route path="/quizApp" element={<QuizApp />} />
       </Routes>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,17 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: "#1F3368",
+        orange: "#F68C1F",
+      },
+      boxShadow: {
+        question: "0px 4px 24px rgba(0,0,0,0.3)",
+        options: "0px 4px 13px rgba(0,0,0,0.5)",
+        quizScore: "0px 4px 104px rgba(0,0,0,0.5)",
+      },
+    },
   },
   plugins: [],
 }
